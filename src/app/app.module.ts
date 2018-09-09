870ebc7ad849d5477e5002cfe153091bbdc76045
Anderson Salles <agsalles@gmail.com>
2018-09-09 15:49:49 -0300

    Adicionado layout  menu, header e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToolbarModule } from 'primeng/toolbar';
+import { AccordionModule } from 'primeng/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToolbarModule,
+    AccordionModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
